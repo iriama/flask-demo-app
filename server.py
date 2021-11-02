e242aefadeb8b3c0eca74b39b439c742e853d89a
iriama <iriama@protonmail.com>
2021-11-02 10:47:36 +0100

    update v2

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    return "Salut toi!"
+    return "Salut toi! (v2)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
